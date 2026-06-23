45d1609007cd2e5352d6a777440d0f58fa53b495
Elyashiv <elyashiv988@gmail.com>
2026-06-23 11:48:49 +0300

    first commit

--- exe.py
+++ exe.py
@@ -2,3 +2,9 @@
 agent={'name': 'Alpha', 'level': 3, 'active': True}
 print(agent)
 print(agent["name"])
+if agent["level"]:
+    print(agent.get("level"))
+else:
+    print(0)
+agent["score"]=95
+print(agent)
